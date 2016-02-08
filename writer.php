74d3aea1d2085b268a3ae3d9d07118eedad5005d
Thomas Keady <thomas.keady@jhu.edu>
2016-02-07 21:22:25 -0500

    Update writer.php

--- writer.php
+++ writer.php
@@ -1,9 +1,8 @@
 if(!empty($_POST['sent'])){
-$data = $_POST['sent'];
-$fname = mktime() . ".txt";//generates random name
+  $data = $_POST['sent'];
 
-$file = fopen("scraps/boring.nothing", 'a'); // append to file
-fwrite($file, $data);
-fwrite($file, "\n");
-fclose($file);
+  $file = fopen("scraps/boring.nothing", 'a'); // append to file
+  fwrite($file, $data);
+  fwrite($file, "\n");
+  fclose($file);
 }
